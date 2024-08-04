3cafed468cfc9f6ea39157f7b4e51d1d32c2783c
vhunany <vhunany@unc.edu>
2024-08-04 11:51:19 -0400

    Adjusting reassignment idea

--- unit00/reassignmentpract.py
+++ unit00/reassignmentpract.py
@@ -1,19 +1,28 @@
+def vars_and_strs(x: str, y: str, z: str, w: str) -> str:
+
+    if not not w != "z":
+        if y == x:
+            print("Mellon")
+        elif not z == "x" and not w != y:
+            print("Carrot")
+            x = "x"
+
+            if x != "x" or z == "y": 
+                print(str("juice"))
+                z = "c"
+            
+        elif x == "y" or z == "w":
+            print("Spinach")
+    else:
+        print("Juice")
+
+    print("!")
+    return ":" + str("P")
+
 x: str = "y"
 y: str = "x"
 z: str = x # "y"
 w: str = z # "y"
 
-if not not w != "z":
-    if y == x:
-        print("Mellon")
-    elif not z == "x" and w != y:
-        print("Carrot")
-        if "x" != "y": 
-            print(str("juice"))
-    elif x == "y" or z == "w":
-        print("Spinach")
-else:
-    print("Juice")
-
-print("!")
-print(":" + str("P"))  # Concatenating then printing the concatenated string and understanding the diff between printing them individually
+if __name__ == "__main__": 
+    print(vars_and_strs(y, z, x, w))
